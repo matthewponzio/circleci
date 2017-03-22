9c2b084d6986bcebbcc6a60dbfd216f6b63973f2
Matthew Ponzio <mponzio@singlestoneconsulting.com>
2017-03-22 09:52:59 -0400

    Updated bootstrap.sh; Created node.json, solo.rb, and circle.yml; Created CFT
    Changes to be committed:
    modified:   bin/bootstrap.sh
    new file:   bin/node.json
    new file:   bin/solo.rb
    new file:   circle.yml
    new file:   cloudformation/chef_deploy.yml

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -15,5 +15,9 @@ dpkg -i chefdk_1.2.22-1_amd64.deb
 ## Download the Chef cookbook
 
 aws s3 cp s3://ponzio-test/circleci.tar.gz .
-cd /opt/chefdk
-tar -xvzf ~/circleci.tar.gz
+mkdir /var/chef
+cd /var/chef
+aws s3 cp s3://ponzio-test/solo.rb .
+aws s3 cp s3://ponzio-test/node.json .
+tar -xvzf /circleci.tar.gz
+chef-solo -c solo.rb -j node.json
