ed37b6b23465fece5d7f9e5003b52461b2c69670
Matthew Ponzio <mponzio@singlestoneconsulting.com>
2017-03-23 13:20:50 -0400

    Updating bootstrap, circle.yml, and chef_deploy CFT

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-## Install unzip
-apt-get install unzip
-
-## Download and install AWS CLI
-curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
-unzip awscli-bundle.zip
-./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
-
 ## Download and install Chef DK
 aws s3 cp s3://ponzio-test/chefdk_1.2.22-1_amd64.deb .
 dpkg -i chefdk_1.2.22-1_amd64.deb
